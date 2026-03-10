180d00f8550b65b9559359ca8d8a5ff033e5e386
szymky <mikaroebers@proton.me>
2026-03-10 22:32:16 +0100

    the file can be read... lesgooo

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
-fn main() {
-    println!("Hello, world!");
+use std::{
+    fs::File,
+    io::{BufRead, BufReader},
+};
+
+use anyhow::Result;
+
+fn main() -> Result<()> {
+    let file = File::open("demo.zi")?;
+    let mut reader = BufReader::new(file);
+    let mut line = String::new();
+
+    while reader.read_line(&mut line)? > 0 {
+        if line.trim().is_empty() {
+            println!("Empty");
+            line.clear();
+            continue;
+        }
+
+        println!("{}", line);
+        line.clear();
+    }
+
+    Ok(())
 }
